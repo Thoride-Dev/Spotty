288015f280c07d47d4511177ee76752d3e4a9099
Kush Dalal <kushdalal007@gmail.com>
2025-02-14 01:05:17 -0700

    Testing more stats

---
+++ Spotty/ContentViews/ExpandedSpottedViews/IcaoCompletionView.swift
@@ -0,0 +1,18 @@
+//
+//  IcaoCompletionView.swift
+//  Spotty
+//
+//  Created by Kush Dalal on 2/13/25.
+//
+
+import SwiftUI
+
+struct IcaoCompletionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IcaoCompletionView()
+}
